6d1f5301889d583c688816d3eff7d5fdc416477b
Hivenimbus <hivenimbus@gmail.com>
2025-10-08 18:51:57 -0300

    botão de audio

--- pages/atendimentos.vue
+++ pages/atendimentos.vue
@@ -511,6 +511,12 @@
                 </button>
                 <button class="text-gray-400 hover:text-gray-600 flex items-center space-x-2 text-sm">
                   <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"/>
+                  </svg>
+                  <span>Áudio</span>
+                </button>
+                <button class="text-gray-400 hover:text-gray-600 flex items-center space-x-2 text-sm">
+                  <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
                   </svg>
                   <span>Emoji</span>
