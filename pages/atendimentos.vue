8d4781d39f525e2f768b2ee586fe0c32149cacd2
Hivenimbus <hivenimbus@gmail.com>
2025-10-08 18:53:13 -0300

    botao de ia

--- pages/atendimentos.vue
+++ pages/atendimentos.vue
@@ -517,6 +517,12 @@
                 </button>
                 <button class="text-gray-400 hover:text-gray-600 flex items-center space-x-2 text-sm">
                   <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"/>
+                  </svg>
+                  <span>Inteligência Artificial</span>
+                </button>
+                <button class="text-gray-400 hover:text-gray-600 flex items-center space-x-2 text-sm">
+                  <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
                   </svg>
                   <span>Emoji</span>
