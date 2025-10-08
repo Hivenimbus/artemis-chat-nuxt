2f6e9f52410f840a4c18daf10480e4003a3feb15
Hivenimbus <hivenimbus@gmail.com>
2025-10-08 18:47:08 -0300

    botao

--- pages/atendimentos.vue
+++ pages/atendimentos.vue
@@ -374,15 +374,17 @@
                 </div>
 
                 <!-- Botão de Resolver -->
-                <button
-                  @click="handleResolveChat"
-                  class="text-gray-400 hover:text-gray-600 transition-colors duration-200"
-                  title="Resolver atendimento"
-                >
-                  <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                  </svg>
-                </button>
+                <div class="relative">
+                  <button
+                    @click="handleResolveChat"
+                    class="text-gray-400 hover:text-gray-600 transition-colors duration-200"
+                    title="Resolver atendimento"
+                  >
+                    <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                    </svg>
+                  </button>
+                </div>
 
                 <!-- Botão Kebab (Mais opções) -->
                 <div class="relative">
